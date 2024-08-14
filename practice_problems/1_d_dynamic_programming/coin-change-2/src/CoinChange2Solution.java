efbe587a96c9a15a83b9c8d01a2ca24dc6e65f84
Samvel Aivazian <ayvazian.samvel@gmail.com>
2024-08-15 00:00:32 +0200

    [NeetCode] Added Simple Versions of Dynamic Programming Tasks of Neetcode

--- practice_problems/1_d_dynamic_programming/coin-change-2/src/CoinChange2Solution.java
+++ practice_problems/1_d_dynamic_programming/coin-change-2/src/CoinChange2Solution.java
@@ -1,27 +1,29 @@
-import java.util.Arrays;
-
 /**
  * Time Complexity: O(m*n)
  * Space Complexity: O(n)
  */
 public class CoinChange2Solution {
 
-    public static int coinChange2(int[] coins, int amount) {
+    public static int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+        dp[0] = 1;
+        for (int i = coins.length - 1; i >= 0; i--) {
+            int[] nextDP = new int[amount + 1];
+            nextDP[0] = 1;
+
+            for (int a = 1; a <= amount; a++) {
+                nextDP[a] = dp[a];
+                if (a - coins[i] >= 0) {
+                    nextDP[a] += nextDP[a - coins[i]];
                 }
             }
+            dp = nextDP;
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount];
     }
 
     public static void main(String[] args) {
-        System.out.println(coinChange2(new int[]{1, 3, 4}, 7));
+        System.out.println(change(7, new int[]{1, 3, 4}));
     }
 
 }
